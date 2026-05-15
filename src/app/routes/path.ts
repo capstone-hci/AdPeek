49522265b1dd3d3a1fc7be993545770982daf70a
earl9rey <earl9rey@naver.com>
2026-05-15 17:28:12 +0900

    feat: path 상수 추가

--- src/app/routes/path.ts
+++ src/app/routes/path.ts
@@ -4,5 +4,7 @@ export const paths = {
   consent: '/participant/step1',
   calibration: '/participant/step2',
   connection: '/participant/step3',
+  ready: '/participant/step4',
+  view: '/participant/step5',
   dashboard: '/marketer/dashboard',
 } as const;

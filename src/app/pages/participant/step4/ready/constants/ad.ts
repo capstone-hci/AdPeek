f054b2fcad3b7779f99e9576601f5f072e4b795a
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-28 15:39:48 +0900

    rename: 제목 수정

--- src/app/pages/participant/step4/ready/constants/ad.ts
+++ src/app/pages/participant/step4/ready/constants/ad.ts
@@ -1,5 +1,5 @@
 /** API 연동 전 더미 광고 데이터 */
 export const DUMMY_AD = {
-  name: '2024 브랜드 리론칭 캠페인',
-  durationSeconds: 30,
+  name: '[제21회 MTN 대한민국 중소기업 광고공모전 출품] 신봤다, 영창에코',
+  durationSeconds: 31,
 } as const;
